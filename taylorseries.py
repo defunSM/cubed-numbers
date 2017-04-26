2c135bb0eb233b2778e71190fd8082823622ac3f
defunsm <sxh8083@g.rit.edu>
2017-04-25 20:41:35 -0400

    added series

---
+++ taylorseries.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+
+import sys, os, math
+
+def factorial(n):
+
+    total = 1
+    for i in range(1,n+1):
+        total = total * i
+
+    print("Factorial of", n, "is :", total)
+    return total
+
+def customseries(iterations):
+    total = 1
+    for i in range(1, iterations+1):
+        total = total +
+    eseries(20)
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
